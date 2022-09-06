b58ce92819bc6e55bed0647cd0f91f21368fccee
mateo <mateotasso@gmail.com>
2022-09-06 13:33:52 -0300

    add swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,3 +49,41 @@ function toggleSkills() {
 skillsHeader.forEach((el) =>{
     el.addEventListener('click', toggleSkills)
 });
+
+/*services modal*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close');
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal');
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () =>{
+        modal(i);
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () =>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal');
+        })
+    })
+})
+
+/*portfolio swiper*/
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination', 
+        clickable: true,
+    },
+});
